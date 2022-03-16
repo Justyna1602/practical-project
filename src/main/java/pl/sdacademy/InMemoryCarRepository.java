0f6760fe870b5da5d92da62f331ea083eaa55c6b
Justyna <justyna.budzioch@gmail.com>
2022-03-16 18:12:14 +0100

    adding InMemoryCarRepository implements CarRepository

---
+++ src/main/java/pl/sdacademy/InMemoryCarRepository.java
@@ -0,0 +1,31 @@
+package pl.sdacademy;
+
+import java.util.List;
+
+public class InMemoryCarRepository implements CarRepository {
+
+    @Override
+    public Car getCarById() {
+        return null;
+    }
+
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void updateCar(Car car) {
+
+    }
+
+    @Override
+    public void deleteCr(Integer id) {
+
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return null;
+    }
+}
