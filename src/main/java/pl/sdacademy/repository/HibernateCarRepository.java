485afa6b3cdd8837c278e73c483cfa36641a886a
Justyna <justyna.budzioch@gmail.com>
2022-03-19 11:54:11 +0100

    Create HibernateCarRepository

---
+++ src/main/java/pl/sdacademy/repository/HibernateCarRepository.java
@@ -0,0 +1,32 @@
+package pl.sdacademy.repository;
+
+import pl.sdacademy.entity.Car;
+
+import java.util.Collection;
+
+public class HibernateCarRepository implements CarRepository{
+    @Override
+    public Car getCarById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void updateCar(Car car) {
+
+    }
+
+    @Override
+    public void deleteCar(Integer id) {
+
+    }
+
+    @Override
+    public Collection<Car> getAllCars() {
+        return null;
+    }
+}
