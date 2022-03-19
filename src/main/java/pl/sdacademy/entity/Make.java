cccbacd5eb4998973c986ba08003d831754f15bc
Justyna <justyna.budzioch@gmail.com>
2022-03-19 11:42:41 +0100

    Using  Lombok in Make klas. Created entity.

--- src/main/java/pl/sdacademy/entity/Make.java
+++ src/main/java/pl/sdacademy/entity/Make.java
@@ -1,31 +1,20 @@
 package pl.sdacademy.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
 public class Make {
+    @Id
     private Integer id;
     private String name;
 
-    public Make(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Make{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
