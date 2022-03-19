2325ce556bd3722e5865a3c96e854e2d1bde2e06
Justyna <justyna.budzioch@gmail.com>
2022-03-19 15:51:18 +0100

    Created MakeUI

---
+++ src/main/java/pl/sdacademy/ui/MakeUI.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.ui;
+
+import pl.sdacademy.repository.MakeRepository;
+
+
+import java.util.Scanner;
+
+public class MakeUI {
+    private MakeRepository modelRepository;
+    private Scanner scanner;
+
+    public MakeUI(MakeRepository modelRepository) {
+        this.scanner = new Scanner(System.in);
+        this.modelRepository = modelRepository;
+
+    }
+}
