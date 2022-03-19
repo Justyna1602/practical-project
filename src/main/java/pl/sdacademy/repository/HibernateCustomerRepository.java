122b98af48aa7565ad0bade6eff0e8495bac2f8f
Justyna <justyna.budzioch@gmail.com>
2022-03-19 13:13:56 +0100

    Implementation methods in CustomerRepository.

--- src/main/java/pl/sdacademy/repository/HibernateCustomerRepository.java
+++ src/main/java/pl/sdacademy/repository/HibernateCustomerRepository.java
@@ -1,32 +1,88 @@
 package pl.sdacademy.repository;
 
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import pl.sdacademy.connection.SessionFactoryProvider;
 import pl.sdacademy.entity.Customer;
 
 import java.util.List;
 
-public class HibernateCustomerRepository implements CustomerRepository{
+public class HibernateCustomerRepository implements CustomerRepository {
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+    private final Logger log = Logger.getLogger(HibernateCustomerRepository.class);
+    Transaction transaction = null;
+
     @Override
     public Customer getCustomerById(Integer id) {
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            Customer customer = session.get(Customer.class, id);
+            transaction.commit();
+            return customer;
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+            log.error(e.getMessage(), e);
+        }
         return null;
     }
 
     @Override
     public void addCustomer(Customer customer) {
-
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(customer);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+            log.error(e.getMessage(), e);
+        }
     }
 
     @Override
     public void update(Customer customer) {
-
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.update(customer);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+            log.error(e.getMessage(), e);
+        }
     }
 
     @Override
     public void delete(Integer id) {
-
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            Customer customer = session.load(Customer.class, id);
+            session.delete(customer);
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+            log.error(e.getMessage(), e);
+        }
     }
 
     @Override
     public List<Customer> getAllCustomer() {
+        List<Customer> customers;
+        try (Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            customers =
+            session.createQuery("SELECT c FROM Customer c", Customer.class).getResultList();
+            transaction.commit();
+            return customers;
+        }catch (HibernateException e) {
+            if(transaction != null)
+                transaction.rollback();
+            log.error(e.getMessage(), e);
+        }
         return null;
     }
 }
