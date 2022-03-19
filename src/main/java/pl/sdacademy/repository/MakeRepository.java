2a55735947d996eec44e07d78ae0277f38a61481
Justyna <justyna.budzioch@gmail.com>
2022-03-19 14:05:24 +0100

    Created MakeRepository

---
+++ src/main/java/pl/sdacademy/repository/MakeRepository.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.repository;
+
+import pl.sdacademy.entity.Make;
+
+import java.util.Collection;
+
+public interface MakeRepository {
+    Make getMakeById(Integer id);
+
+    void addCar(Make make);
+
+    void updateCar(Make make);
+
+    void deleteCar(Integer id);
+
+    Collection<Make> getAllCars();
+
+}
