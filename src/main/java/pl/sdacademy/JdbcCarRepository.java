6e754b9817f89c3663c8dfe68c44cb01775c31e8
Justyna <justyna.budzioch@gmail.com>
2022-03-19 08:54:58 +0100

    adding JdbcCarRepository and dependence hibernate

--- src/main/java/pl/sdacademy/JdbcCarRepository.java
+++ src/main/java/pl/sdacademy/JdbcCarRepository.java
@@ -1,8 +1,17 @@
 package pl.sdacademy;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Collection;
 
 public class JdbcCarRepository implements CarRepository{
+    private static PreparedStatement preparedStatement;
+    private Connection connection;
+
+    public JdbcCarRepository(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public Car getCarById(Integer id) {
         return null;
